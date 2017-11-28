5aaa7b2d721348018f3b8ece2f2088ec05ef898a
Thành Đạt Nguyễn <gorila113114@gmail.com>
2017-11-28 15:36:59 +0700

    add all DTO cs

---
+++ QuanLyBanCaPhe/DTO/Ban_DTO.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class Ban_DTO
+    {
+        public string Ma_Ban { get; set; }
+        public string Tenn_Ban { get; set; }
+        public string Ma_KV { get; set; }
+        public string Tinh_Trang { get; set; }
+        public int So { get; set; }        
+    }
+}
---
+++ QuanLyBanCaPhe/DTO/NhanVien_DTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DTO
+{
+    public class NhanVien_DTO
+    {
+        public string MaNV { get; set; }
+        public string TenNV { get; set; }
+        public string GioiTinh { get; set; }
+        public string DiaChi { get; set; }
+        public string SDT { get; set; }
+        public string Email { get; set; }
+        public DateTime NgaySinh { get; set; }
+        public string ChucVu { get; set; }
+        public DateTime NgayVaoLam { get; set; }
+        public int Luong { get; set; }
+    }
+}
