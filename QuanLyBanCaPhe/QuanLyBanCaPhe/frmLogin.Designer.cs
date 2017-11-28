5fc8798d7d69344bce669a88ea86475e77e18e81
Thành Đạt Nguyễn <gorila113114@gmail.com>
2017-11-28 15:25:24 +0700

    change form

--- QuanLyBanCaPhe/QuanLyBanCaPhe/frmLogin.Designer.cs
+++ QuanLyBanCaPhe/QuanLyBanCaPhe/frmLogin.Designer.cs
@@ -28,9 +28,17 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.SuspendLayout();
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(854, 261);
+            this.Name = "frmLogin";
             this.Text = "frmLogin";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
